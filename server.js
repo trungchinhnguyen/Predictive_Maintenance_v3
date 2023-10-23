ea11e0e346913a7018d549e243bf800191a57457
Nguyen Trung Chinh <nguyentrungchinh@Nguyens-MacBook-Pro.local>
2023-10-23 10:17:12 +0700

    second commit

--- server.js
+++ server.js
@@ -1,12 +1,30 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const path = require('path')
+const dotenv = require('dotenv')
+dotenv.config()
+
 const cors = require('cors');
 
-app.use(cors());
 
-app.get('/upload', function(req, res) {
-    res.status(200).json({ result: true, msg: 'GET request to the homepage' });
-});
+
+const app = express()
+const port = process.env.PORT || 10000
+
+
+
+const buildPath = path.join(__dirname, 'build')
+
+
+app.use(express.static(buildPath))
+app.use(express.json())
+app.use(cors())
+
+
+// gets the static files from the build folder
+app.get('*', (req, res) => {
+  res.sendFile(path.join(buildPath, 'index.html'))
+})
+
 
 app.post("/upload", (req, res) => {
     setTimeout(() => {
@@ -21,6 +39,8 @@ app.delete("/upload", (req, res) => {
 });
 
 
-app.listen(3002, () => {
-    console.log(`Server running on port 3002`)
-});
+// Showing that the server is online and running and listening for changes
+app.listen(port, () => {
+  console.log(`Server is online on port: ${port}`)
+})
+
